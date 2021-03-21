2a010e3f69200d12d9570722a4a343de8f60ce95
fanchaokong <finlaykong@126.com>
2021-03-21 19:14:05 +0800

    first commit

---
+++ src/main/java/com/example/retenalCar/RetenalCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.retenalCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetenalCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetenalCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/retenalCar/RetenalCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retenalCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetenalCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
