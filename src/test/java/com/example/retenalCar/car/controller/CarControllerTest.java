ec66b910f0ba1ab30b09dc12911e6f10bf59d233
fanchaokong <finlaykong@126.com>
2021-03-21 19:30:32 +0800

    fanchao: add test of car controller

---
+++ src/test/java/com/example/retenalCar/car/controller/CarControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.retenalCar.car.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = CarController.class)
+@ExtendWith(SpringExtension.class)
+public class CarControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static final String GET_CAR_URL = "api/v1/rental/available-cars";
+    @Test
+    void should_return_cars_when_get_car_given_start_date_and_end_date() throws Exception {
+        mockMvc.perform(get(GET_CAR_URL)
+                .param("startDate", "2021-03-23")
+                .param("endDate", "2021-03-25"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    void should_return_400_when_call_api_given_only_start_date() throws Exception {
+        mockMvc.perform(get(GET_CAR_URL)
+                .param("startDate", "2021-03-23"))
+                .andExpect(status().isBadRequest());
+    }
+}
